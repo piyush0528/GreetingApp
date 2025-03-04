746f9f2552f79a21ebcbb1fd74851f176ffcfa1f
piyush0528 <tiwaripiyush262@gmail.com>
2025-03-04 14:28:47 +0530

    to find greeting message by id

--- src/main/java/com/piyush/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/piyush/GreetingApp/controller/GreetingController.java
@@ -60,4 +60,9 @@ public class GreetingController {
             @RequestParam(required = false) String lastName) {
         return greetingService.saveGreeting(firstName, lastName);
     }
+
+    @GetMapping("getId/{id}")
+    public Greeting getGreetingById(@PathVariable Long id){
+        return greetingService.getGreetById(id);
+    }
 }
--- src/main/java/com/piyush/GreetingApp/service/GreetingService.java
+++ src/main/java/com/piyush/GreetingApp/service/GreetingService.java
@@ -40,4 +40,7 @@ public class GreetingService {
         Greeting greeting = new Greeting(message);
         return greetingRepository.save(greeting);
     }
+    public Greeting getGreetById(Long id) {
+        return greetingRepository.findById(id).orElseThrow(()->new RuntimeException("Greeting not found with id: " + id));
+    }
 }
